b2186574e3485ae0416808d3db5fc37ba429423d
Bitsonwheels <51041273+Bitsonwheels@users.noreply.github.com>
2024-11-30 13:53:01 +0100

    add Homebrew Install
    Homebrew Install with own Linuxbrew user

--- build.sh
+++ build.sh
@@ -7,6 +7,14 @@ RELEASE="$(rpm -E %fedora)"
 
 ### Install packages
 
+# Install Homebrew
+cat <<EOT >> ~/.profile
+#Linuxbrew
+test -d ~/.linuxbrew && eval \$(~/.linuxbrew/bin/brew shellenv)
+test -d /home/linuxbrew/.linuxbrew && eval \$(/home/linuxbrew/.linuxbrew/bin/brew shellenv)
+test -r ~/.bash_profile && echo "eval \\\$(\$(brew --prefix)/bin/brew shellenv)" >>~/.bash_profile
+echo "eval \\\$(\$(brew --prefix)/bin/brew shellenv)" >>~/.profile
+
 # Packages can be installed from any enabled yum repo on the image.
 # RPMfusion repos are available by default in ublue main images
 # List of rpmfusion packages can be found here:
@@ -16,7 +24,7 @@ RELEASE="$(rpm -E %fedora)"
 rpm-ostree install screen
 
 # this would install a package from rpmfusion
-# rpm-ostree install vlc
+rpm-ostree install vlc
 
 #### Example for enabling a System Unit File
 
