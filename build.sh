ad308b1e76e7af08191098422acbf87c8a854bdc
Bitsonwheels <51041273+Bitsonwheels@users.noreply.github.com>
2024-11-30 16:29:57 +0100

    add Workaround AMD/ATI/SUN XT Cards
    add Radeon Ring Test Failed Error Workaround for older AMD/ATI/SUN XT Cards
    
    rpm-ostree kargs --append=radeon.modeset=0

--- build.sh
+++ build.sh
@@ -4,6 +4,9 @@ set -ouex pipefail
 
 RELEASE="$(rpm -E %fedora)"
 
+# add Radeon Ring Test Failed Error Workaround for older AMD/ATI/SUN XT Cards
+rpm-ostree kargs --append=radeon.modeset=0
+
 
 ### Install packages
 
